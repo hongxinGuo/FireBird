06c34ff0bdbf10958fbe9fce601da58f2f066afc
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2019-07-07 05:54:34 +0800

    +1

---
+++ UnitTest/SystemStatusTest.cpp
@@ -0,0 +1,15 @@
+#include"stdafx.h"
+#include"pch.h"
+
+#include"SystemStatus.h"
+/*
+namespace StockAnalysisTest {
+  TEST(SystemStatusTest, TestIsCalculateRSInProcess) {
+    CSystemStatus status;
+    status.SetCalculateRSInProcess(true);
+    EXPECT_TRUE(status.IsCalculateRSInProcess());
+    status.SetCalculateRSInProcess(false);
+    EXPECT_FALSE(status.IsCalculateRSInProcess());
+  }
+}
+*/
