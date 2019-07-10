7156313c69d4251d76283b0a3664112f450f3286
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2019-07-10 19:17:29 +0800

    完成第二个挂单测试函数。

--- UnitTest/CalculateRTDataTest.cpp
+++ UnitTest/CalculateRTDataTest.cpp
@@ -22,22 +22,22 @@ namespace StockAnalysisTest {
       CurrentData.m_lPSell[2] = dS3; CurrentData.m_lVSell[2] = lS3;
       CurrentData.m_lPSell[1] = dS2; CurrentData.m_lVSell[1] = lS2;
       CurrentData.m_lPSell[0] = dS1; CurrentData.m_lVSell[0] = lS1;
-      CurrentData.m_lPBuy[4] = dS5; CurrentData.m_lVBuy[4] = lS5;
-      CurrentData.m_lPBuy[3] = dS4; CurrentData.m_lVBuy[3] = lS4;
-      CurrentData.m_lPBuy[2] = dS3; CurrentData.m_lVBuy[2] = lS3;
-      CurrentData.m_lPBuy[1] = dS2; CurrentData.m_lVBuy[1] = lS2;
-      CurrentData.m_lPBuy[0] = dS1; CurrentData.m_lVBuy[0] = lS1;
+      CurrentData.m_lPBuy[4] = dB5; CurrentData.m_lVBuy[4] = lB5;
+      CurrentData.m_lPBuy[3] = dB4; CurrentData.m_lVBuy[3] = lB4;
+      CurrentData.m_lPBuy[2] = dB3; CurrentData.m_lVBuy[2] = lB3;
+      CurrentData.m_lPBuy[1] = dB2; CurrentData.m_lVBuy[1] = lB2;
+      CurrentData.m_lPBuy[0] = dB1; CurrentData.m_lVBuy[0] = lB1;
 
-      LastData.m_lPSell[4] = dS5; LastData.m_lVSell[4] = lS5;
-      LastData.m_lPSell[3] = dS4; LastData.m_lVSell[3] = lS4;
-      LastData.m_lPSell[2] = dS3; LastData.m_lVSell[2] = lS3;
-      LastData.m_lPSell[1] = dS2; LastData.m_lVSell[1] = lS2;
-      LastData.m_lPSell[0] = dS1; LastData.m_lVSell[0] = lS1;
-      LastData.m_lPBuy[4] = dS5; LastData.m_lVBuy[4] = lS5;
-      LastData.m_lPBuy[3] = dS4; LastData.m_lVBuy[3] = lS4;
-      LastData.m_lPBuy[2] = dS3; LastData.m_lVBuy[2] = lS3;
-      LastData.m_lPBuy[1] = dS2; LastData.m_lVBuy[1] = lS2;
-      LastData.m_lPBuy[0] = dS1; LastData.m_lVBuy[0] = lS1;
+      LastData.m_lPSell[4] = dS5l; LastData.m_lVSell[4] = lS5l;
+      LastData.m_lPSell[3] = dS4l; LastData.m_lVSell[3] = lS4l;
+      LastData.m_lPSell[2] = dS3l; LastData.m_lVSell[2] = lS3l;
+      LastData.m_lPSell[1] = dS2l; LastData.m_lVSell[1] = lS2l;
+      LastData.m_lPSell[0] = dS1l; LastData.m_lVSell[0] = lS1l;
+      LastData.m_lPBuy[4] = dB5l; LastData.m_lVBuy[4] = lB5l;
+      LastData.m_lPBuy[3] = dB4l; LastData.m_lVBuy[3] = lB4l;
+      LastData.m_lPBuy[2] = dB3l; LastData.m_lVBuy[2] = lB3l;
+      LastData.m_lPBuy[1] = dB2l; LastData.m_lVBuy[1] = lB2l;
+      LastData.m_lPBuy[0] = dB1l; LastData.m_lVBuy[0] = lB1l;
     }
 
   public:
@@ -48,11 +48,18 @@ namespace StockAnalysisTest {
     double dPrice;
   };
 
+  // 没有任何变化，无成交
   GuaDanData GuaDan1(0, __NO_TRANSACTION__, 0,
-                    1005, 10000, 1004, 10000, 1003, 10000, 1002, 10000, 1001, 10000,
-                    1000, 10000, 999, 10000, 998, 10000, 997, 10000, 996, 10000,
-                    1005, 10000, 1004, 10000, 1003, 10000, 1002, 10000, 1001, 10000,
-                    1000, 10000, 999, 10000, 998, 10000, 997, 10000, 996, 10000);
+    1005, 10000, 1004, 10000, 1003, 10000, 1002, 10000, 1001, 10000,
+    1000, 10000, 999, 10000, 998, 10000, 997, 10000, 996, 10000,
+    1005, 10000, 1004, 10000, 1003, 10000, 1002, 10000, 1001, 10000,
+    1000, 10000, 999, 10000, 998, 10000, 997, 10000, 996, 10000);
+  // 无成交，出现新的挂单位置
+  GuaDanData GuaDan2(1, __NO_TRANSACTION__, 0,
+    1025, 10000, 1014, 20000, 1003, 10000, 1002, 10000, 1001, 10000,
+    1000, 10000, 999, 10000, 998, 10000, 987, 20000, 986, 10000,
+    1005, 10000, 1004, 10000, 1003, 10000, 1002, 10000, 1001, 10000,
+    1000, 10000, 999, 10000, 998, 10000, 997, 10000, 996, 10000);
 
   class RTDataGuaDanTest : public::testing::TestWithParam<GuaDanData *>
   {
@@ -95,18 +102,29 @@ namespace StockAnalysisTest {
     CStock m_stock;
   };
 
-  INSTANTIATE_TEST_CASE_P(TestGuaDanData, RTDataGuaDanTest, testing::Values(&GuaDan1));
+  INSTANTIATE_TEST_CASE_P(TestGuaDanData, RTDataGuaDanTest, testing::Values(&GuaDan1,&GuaDan2));
 
   TEST_P(RTDataGuaDanTest, TestGuaDan) {
     EXPECT_FALSE(m_stock.IsStartCalculating());
     m_stock.AnalysisingGuaDan(pCurrentData, pLastData, iType, dPrice);
     switch (iCount) {
-    case 0:
+    case 0: // 无成交，无变化
       EXPECT_EQ(m_stock.GetCancelBuyVolume(), 0);
       EXPECT_EQ(m_stock.GetCancelSellVolume(), 0);
       for (int i = 0; i < 20; i++) {
         EXPECT_EQ(m_stock.GetGuaDan(990 + i), 10000);
       }
+      break;
+    case 1: // 无成交，出现新的挂单位置：1025， 986
+      EXPECT_EQ(m_stock.GetCancelBuyVolume(), 0);
+      EXPECT_EQ(m_stock.GetCancelSellVolume(), 0);
+      for (int i = 0; i < 20; i++) {
+        EXPECT_EQ(m_stock.GetGuaDan(990 + i), 10000);
+      }
+      EXPECT_EQ(m_stock.GetGuaDan(1025), 10000);
+      EXPECT_EQ(m_stock.GetGuaDan(1014), 20000);
+      EXPECT_EQ(m_stock.GetGuaDan(987), 20000);
+      EXPECT_EQ(m_stock.GetGuaDan(986), 10000);
     }
   }
 }
