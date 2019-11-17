edeb45819164b7fecea6f7dbc432c7d208191a1b
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2019-11-17 14:44:09 +0800

    googletest升级后，测试文件编译时出现错误。暂时不升级了。

--- GoogleUnitTest/main.cpp
+++ GoogleUnitTest/main.cpp
@@ -18,7 +18,8 @@ int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   // gtest takes ownership of the TestEnvironment ptr - we don't delete it.
   ::testing::AddGlobalTestEnvironment(new TestEnvironment);
-  //ASSERT_FALSE(gl_fNormalMode);
+
+  EXPECT_FALSE(gl_fNormalMode);
 
   return RUN_ALL_TESTS();
 }
