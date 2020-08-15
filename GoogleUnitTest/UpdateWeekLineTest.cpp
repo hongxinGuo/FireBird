67e66ba24383b85332094c0d3651c506459c0203
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2020-08-16 07:24:06 +0800

    +1

---
+++ GoogleUnitTest/UpdateWeekLineTest.cpp
@@ -0,0 +1,38 @@
+#include"stdafx.h"
+#include"pch.h"
+
+#include"globedef.h"
+
+namespace StockAnalysisTest {
+  struct UpdateWeekLineDataStr {
+    UpdateWeekLineDataStr() {
+    }
+  public:
+    int m_iCount;
+    CString m_strData;
+  };
+
+  UpdateWeekLineDataStr Data1;
+
+  class UpdateWeekLineDataStrTest : public::testing::TestWithParam<UpdateWeekLineDataStr*> {
+  protected:
+    virtual void SetUp(void) override {
+      ASSERT_FALSE(gl_fNormalMode);
+      UpdateWeekLineDataStr* pData = GetParam();
+      m_iCount = pData->m_iCount;
+      m_strCode = pData->m_strData;
+    }
+
+    virtual void TearDown(void) override {
+      // clearup
+    }
+
+  public:
+    int m_iCount;
+    CString m_strCode;
+  };
+
+  INSTANTIATE_TEST_SUITE_P(TestUpdateWeekLine, UpdateWeekLineDataStrTest,
+                           testing::Values(&Data1
+                           ));
+}
