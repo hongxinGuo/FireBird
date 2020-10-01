64501d6fb41da914e6f039f262324b30cb33504f
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2020-10-01 12:50:18 +0800

    +1

--- StockAnalysis/resource.h
+++ StockAnalysis/resource.h
@@ -120,7 +120,7 @@
 #define ID_VIEW_STATUS_BAR2             59394
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        318
