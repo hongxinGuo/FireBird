1c3a6c6d5a957fc8b9a1c342a8b70b459b2d4f26
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2020-12-22 18:51:39 +0800

    +1

--- StockAnalysis/source/AmericaMarket/ThreadUpdateStakeDB.cpp
+++ StockAnalysis/source/AmericaMarket/ThreadUpdateStakeDB.cpp
@@ -2,7 +2,9 @@
 
 UINT ThreadUpdateStakeDB(not_null<CAmericaMarket*> pMarket) {
   gl_SaveAmericaOneStockDayLine.Wait();
+  gl_SaveEPSSurprise.Wait();
   pMarket->UpdateStakeDB();
+  gl_SaveEPSSurprise.Signal();
   gl_SaveAmericaOneStockDayLine.Signal();
 
   return 37;
