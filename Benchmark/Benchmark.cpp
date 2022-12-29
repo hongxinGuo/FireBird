e8476a181d0e44e5ba18f909e632919971ff096e
hongxinGuo <46558818+hongxinGuo@users.noreply.github.com>
2022-12-29 11:47:49 +0800

    +1

--- Benchmark/Benchmark.cpp
+++ Benchmark/Benchmark.cpp
@@ -6,11 +6,11 @@
 #pragma comment(lib, "shlwapi.lib")
 
 #ifdef _DEBUG
-#pragma comment(lib, "debug/benchmark.lib")
-#pragma comment(lib, "debug/benchmark_main.lib")
+#pragma comment(lib, "d/benchmark.lib")
+#pragma comment(lib, "d/benchmark_main.lib")
 #else
-#pragma comment(lib, "release/benchmark.lib")
-#pragma comment(lib, "release/benchmark_main.lib")
+#pragma comment(lib, "r/benchmark.lib")
+#pragma comment(lib, "r/benchmark_main.lib")
 #endif
 
 BENCHMARK_MAIN();
